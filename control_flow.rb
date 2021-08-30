17e9fa74211e47a7c4b0017fb8e5417cdd4107c9
Sam Shearer <sshearer101@gmail.com>
2021-08-30 15:19:37 -0600

    commit tree 92738d0293a48a1083a41b633774a23a3b487265

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,19 @@
-def admin_login(username, password)
-  # your code here
-end
+# def admin_login(username, password)
+#   if (username == "admin" || username = "ADMIN") && password = "12345"
+#     "Access granted"
+#   else
+#     "Access denied"
+# end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && <= 65
+    "It's a little chilly out there"
+  elsif temperature > 85
+    "It's too dang hot our there"
+  else 
+    "It's perfect out there!"
 end
 
 def fizzbuzz(num)
